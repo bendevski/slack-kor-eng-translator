bd6895dc6dffd7b52d562cfbd09585ca986d2bac
bendevski <bende@localhost.localdomain>
2021-08-28 02:15:12 +0900

    fix bracket

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { default: axios } = require('axios');
 const express = require('express');
-
+const {TRANS_HEADERS} = require('./env.json');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded())
@@ -10,8 +11,15 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req,res) => {
-	received.push(JSON.parse(req.body.payload))
+	const {text} = JSON.parse(req.body.payload)
 	res.status(200).send();
+	const body = {
+		"source": "ko",
+		"target": "en",
+		"text": text
+	}
+	const translated = await axios.post("https://openapi.naver.com/v1/papago/n2mt",headers=TRANS_HEADERS, body=body);
+	received.push(translated);
 });
 
 app.listen(process.env.PORT);
