0e1406c15c45e8eb199af4857badb25343b758e9
bendevski <bende@localhost.localdomain>
2021-08-27 23:30:05 +0900

    initial commit

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+
+let received = []
+
 app.get("/", (req, res) => {
-	console.log(req.body);
+	setTimeout(()=>console.log(req.body), 10000);
 });
+
+app.post("/", (req,res) => {
+	received.push(res.body);
+};
 
 app.listen(8000);
