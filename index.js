61e720a297ccc19debb9ec1a61f719f792b7f00d
bendevski <bende@localhost.localdomain>
2021-08-28 00:48:56 +0900

    fix bracket

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req,res) => {
-	received.push(req.params);
+	received.push([req.params, req.body, req.query]);
 	res.send({"challenge": req.params.challenge});
 });
 
